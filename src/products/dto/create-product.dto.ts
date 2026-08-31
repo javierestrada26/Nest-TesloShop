04838ca9ec2a66cabef3e298725f8f410c44a543
javierestrada26 <javiestrada14@hotmail.com>
2026-08-31 11:59:41 -0500

    feat: enhance CreateProductDto with validation rules and add class-transformer and class-validator dependencies

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,34 @@
-export class CreateProductDto {}
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, Min } from "class-validator";
+
+
+export class CreateProductDto {
+    @IsString()
+    @Min(1)
+    title!:string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?:number;
+
+    @IsString()
+    @IsOptional()
+    description?:string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsOptional()
+    @IsPositive()
+    stock?:number;
+
+    @IsString({each:true})
+    @IsArray()
+    sizes!: string[];
+
+    @IsIn(['men','women','kid','unisex'])
+    gender!:string;
+
+}
