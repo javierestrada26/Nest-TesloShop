00913e413b31f95342425353227cd581493d1084
javierestrada26 <javiestrada14@hotmail.com>
2026-08-31 17:26:08 -0500

    feat: enhance ProductsService with NotFoundException handling and update findOne method logic

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -18,7 +18,7 @@ export class ProductsService {
 
   async create(createProductDto: CreateProductDto) {
     try {
-      
+
       const product = this.productRepository.create(createProductDto);
       await this.productRepository.save(product);
 
@@ -30,19 +30,25 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find({})
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+
+    const product = await this.productRepository.findOneBy({id});
+    if(!product) 
+      throw new NotFoundException(`Product with id ${id} not found`)
+    return product
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id)
+    
+    await this.productRepository.remove(product);
   }
 
 
