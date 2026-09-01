e98eff97369c10d9ed68975bbd696810e8cdaf1f
javierestrada26 <javiestrada14@hotmail.com>
2026-09-01 17:17:20 -0500

    feat: enhance product retrieval by adding eager loading for images and implementing findOnePlain method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -40,14 +40,20 @@ export class ProductsService {
     }
   }
 
-  findAll(paginationDto: PaginationDto) {
+  async findAll(paginationDto: PaginationDto) {
     const {limit =10, offset=0} = paginationDto
-    return this.productRepository.find({
+    const products = await this.productRepository.find({
       take:limit,
       skip:offset,
-
-      //TODO: relaciones
+      relations:{
+        images: true,
+      }
     })
+
+    return products.map((product) =>({
+      ...product,
+      images:product.images!.map(img => img.url)
+    }))
   }
 
   async findOne(term: string) {
@@ -57,17 +63,28 @@ export class ProductsService {
     if(isUUID(term)){
       product =await this.productRepository.findOneBy({id:term})
     }else{
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('prod');
       product = await queryBuilder
         .where(`UPPER(title) =:title or slug =:slug`,{
           title:term.toUpperCase(),
           slug: term.toLowerCase(),
-        }).getOne();
+        })
+        .leftJoinAndSelect('prod.images','prodImages')
+        .getOne();
     }
 
     if(!product) 
-      throw new NotFoundException(`Product with id ${term} not found`)
+      throw new NotFoundException(`Product with id ${term} not found`);
+
     return product
+  }
+
+  async findOnePlain(term:string){
+    const {images =[], ...rest} =  await this.findOne(term);
+    return{
+      ...rest,
+      images: images.map(image => image.url)
+    }
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
