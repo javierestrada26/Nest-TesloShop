30b767062e6bd943a99b346c4d62916d1c6be913
javierestrada26 <javiestrada14@hotmail.com>
2026-09-02 16:38:05 -0500

    feat: implement image upload functionality with disk storage and add .gitkeep files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 import { fileFilter } from './helpers/fileFilter.helper';
 import 'multer';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
@@ -12,6 +13,10 @@ export class FilesController {
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
+      //limits:{fileSize: 1000}
+      storage:diskStorage({
+        destination:'./static/uploads'
+      })
     }),
   )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
