05b291366f75f0352c8770774b2f1867243d5eaa
javierestrada26 <javiestrada14@hotmail.com>
2026-09-03 10:07:06 -0500

    Refactor code structure for improved readability and maintainability

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -4,12 +4,16 @@ import { FilesService } from './files.service';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
 import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -37,7 +41,7 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    const secureUrl= `${file.filename}`
+    const secureUrl= `${this.configService.get('HOST_API')}/files/product/${file.filename}`
 
     return {
       secureUrl
