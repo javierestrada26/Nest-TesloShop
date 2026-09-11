d7efc867213e3fe8fc0e2f63f1a1340d4605271d
javierestrada26 <javiestrada14@hotmail.com>
2026-09-11 12:57:37 -0500

    feat: establish relationship between User and Product entities with ManyToOne association

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,6 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
+import { User } from "../../auth/entities/users.entity";
 
 
 @Entity({ name:'products'})
@@ -56,6 +57,13 @@ export class Product {
 
     )
     images?: ProductImage[];
+
+    @ManyToOne(
+        ()=>User,
+        (user) =>user.product,
+        {eager:true}
+    )
+    user!:User;
 
 
 
