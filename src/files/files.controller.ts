cfc36c99166f3f0792a609c50d56e332604edf61
javierestrada26 <javiestrada14@hotmail.com>
2026-09-11 16:45:58 -0500

    feat: add ApiTags and ApiResponse decorators for improved Swagger documentation across controllers

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -5,9 +5,10 @@ import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
 import type { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { ApiTags } from '@nestjs/swagger';
 
 
-
+@ApiTags('Files - Get and Upload')
 @Controller('files')
 export class FilesController {
   constructor(
