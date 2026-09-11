e4ce6e8fce345d60c32967d78e6fb2f8d949b9e2
javierestrada26 <javiestrada14@hotmail.com>
2026-09-10 23:29:53 -0500

    feat: add UserRoleGuard for role-based access control in private routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 
 import { User } from './entities/users.entity';
 import { GetUser, RawHeaders } from './decorators';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+
 
 
 
@@ -40,4 +42,17 @@ export class AuthController {
     }
   }
 
+
+  @Get('private2')
+  @SetMetadata('roles',['admin','superUser'])
+  @UseGuards(AuthGuard(),UserRoleGuard)
+  privateRoute2(
+    @GetUser() user:User
+  ){
+
+    return{
+      ok:true,
+      user
+    }
+  }
 }
