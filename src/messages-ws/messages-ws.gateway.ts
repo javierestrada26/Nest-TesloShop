3ba97387e6ac78543f26f8b5958f7c4662385e62
javierestrada26 <javiestrada14@hotmail.com>
2026-09-14 15:01:34 -0500

    feat: update WebSocket gateway to emit client updates and modify client count retrieval in service

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,21 +1,25 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService} from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({cors:true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+  
+  @WebSocketServer() wss!: Server;
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
 
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
 
-    console.log({conectados: this.messagesWsService.getConnectedClients()});
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
 
 
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
 
 
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -21,7 +21,7 @@ export class MessagesWsService {
         delete this.connectedClients[clientId];
     }
 
-    getConnectedClients():number {
-        return Object.keys(this.connectedClients).length;
+    getConnectedClients():string[]{
+        return Object.keys(this.connectedClients);
     }
 }
