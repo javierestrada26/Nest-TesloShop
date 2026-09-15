5e8062ba0f0a323800ea7e998d63c3a954df0492
javierestrada26 <javiestrada14@hotmail.com>
2026-09-14 21:54:59 -0500

    feat: add JWT verification in WebSocket connection handling and include AuthModule in MessagesWsModule

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,16 +2,29 @@ import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGa
 import { MessagesWsService} from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({cors:true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
   
   @WebSocketServer() wss!: Server;
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
+  ) {}
 
 
   handleConnection(client: Socket) {
     const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+    } catch (error) {
+
+      client.disconnect();
+      return;
+    }
     this.messagesWsService.registerClient(client);
 
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
