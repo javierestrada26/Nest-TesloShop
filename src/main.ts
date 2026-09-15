0489cb8b6de9dceb94251c5929b245c5da5e6e99
javierestrada26 <javiestrada14@hotmail.com>
2026-09-14 22:56:18 -0500

    feat: update environment template and enhance database connection handling with SSL support

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
